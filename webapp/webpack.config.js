9983818c02e6518f2bf859072d718bbafb117e1e
Nicolas Mora <mail@babelouest.org>
2017-04-05 22:49:48 -0400

    Set prod mode prefix to /app , this makes dev mode to crash, gotta fix that too now...

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -59,7 +59,7 @@ module.exports = function makeWebpackConfig() {
    */
   config.output = isTest ? {} : {
     path: root('dist'),
-    publicPath: isProd ? '/' : 'http://localhost:8080/',
+    publicPath: isProd ? 'app/' : 'http://localhost:8080/',
     filename: isProd ? 'js/[name].[hash].js' : 'js/[name].js',
     chunkFilename: isProd ? '[id].[hash].chunk.js' : '[id].chunk.js'
   };
