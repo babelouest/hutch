aee09c2617f52a4fb69664e11ca32d3af5b38642
Nicolas Mora <github@babelouest.org>
2017-04-18 11:06:37 -0400

    Set front-end licence to AGPL

--- webapp/src/app/shared/hutch-crypto.service.ts
+++ webapp/src/app/shared/hutch-crypto.service.ts
@@ -1,4 +1,24 @@
 /**
+ *
+ * Hutch - Password and private data locker
+ *
+ * Application front-end
+ *
+ * Copyright 2017 Nicolas Mora <mail@babelouest.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * License as published by the Free Software Foundation;
+ * version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  * Algorithms available for hutch are AES-GCM, AES-CTR, AES-CBC
  *
  * Algorithm recommended by https://github.com/diafygi/webcrypto-examples is AES-GCM, with key length of 256 bits
