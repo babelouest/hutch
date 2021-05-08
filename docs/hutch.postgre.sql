05c2202c98ee7d46d814913e7801f0a0709d5c9f
Nicolas Mora <github@babelouest.org>
2021-05-08 10:58:25 -0400

    Rework sql scripts, version 2.0

--- docs/hutch.postgre.sql
+++ docs/hutch.postgre.sql
@@ -1,5 +1,5 @@
 -- ----------------------------------------------------- --
---                MariaDB Database                       --
+--                PostgreSQL Database                    --
 -- Initialize Hutch Database for the backend server      --
 -- The administration client app                         --
 -- Copyright 2021 Nicolas Mora <mail@babelouest.org>     --
@@ -13,49 +13,49 @@ DROP TABLE IF EXISTS h_profile;
 
 -- Contain a user profile with a message and a picture choosen by the user to authenticate the server
 CREATE TABLE h_profile (
-  hp_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  hp_id SERIAL PRIMARY KEY,
   hp_sub VARCHAR(128) NOT NULL,
   hp_name VARCHAR(256),
   hp_fortune VARCHAR(512),
   hp_picture MEDIUMBLOB,
   hp_sign_kid VARCHAR(128),
-  hp_deleted TINYINT DEFAULT 0,
-  hp_last_updated TIMESTAMP
+  hp_deleted SMALLINT DEFAULT 0,
+  hp_last_updated TIMESTAMPZ
 );
 CREATE INDEX i_hp_username ON h_profile(hp_sub);
 
 -- Meta data about the safe
 CREATE TABLE h_safe (
-  hs_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  hs_id SERIAL PRIMARY KEY,
   hp_id INT(11) NOT NULL,
   hs_name VARCHAR(128) NOT NULL,
   hs_display_name VARCHAR(512),
   hs_enc_type VARCHAR(128),
-  hs_deleted TINYINT DEFAULT 0,
-  hs_last_updated TIMESTAMP,
+  hs_deleted SMALLINT DEFAULT 0,
+  hs_last_updated TIMESTAMPZ,
   FOREIGN KEY(hp_id) REFERENCES h_profile(hp_id) ON DELETE CASCADE
 );
 
 -- Safe master key
 CREATE TABLE h_key (
-  hk_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  hk_id SERIAL PRIMARY KEY,
   hs_id INT(11) NOT NULL,
   hk_type VARCHAR(128),
   hk_name VARCHAR(128) NOT NULL,
   hk_display_name VARCHAR(512),
   hk_data MEDIUMBLOB, --  The key is protected in a way known by the client and the user only
-  hk_deleted TINYINT DEFAULT 0,
-  hk_last_updated TIMESTAMP,
+  hk_deleted SMALLINT DEFAULT 0,
+  hk_last_updated TIMESTAMPZ,
   FOREIGN KEY(hs_id) REFERENCES h_safe(hs_id) ON DELETE CASCADE
 );
 
 -- Coin stored in the safe
 CREATE TABLE h_coin (
-  hc_id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  hc_id SERIAL PRIMARY KEY,
   hs_id INT(11) NOT NULL,
   hc_name VARCHAR(128) NOT NULL,
   hc_data MEDIUMBLOB, --  The usable data is encoded in a JWE, so only the end-user can access it if she/he has the master key
-  hc_deleted TINYINT DEFAULT 0,
-  hc_last_updated TIMESTAMP,
+  hc_deleted SMALLINT DEFAULT 0,
+  hc_last_updated TIMESTAMPZ,
   FOREIGN KEY(hs_id) REFERENCES h_safe(hs_id) ON DELETE CASCADE
 );
--- docs/hutch.sqlite3.sql
+++ docs/hutch.sqlite3.sql
@@ -1,68 +1,61 @@
--- SQlite3 init script
+-- ----------------------------------------------------- --
+--                SQLite3 Database                       --
+-- Initialize Hutch Database for the backend server      --
+-- The administration client app                         --
+-- Copyright 2021 Nicolas Mora <mail@babelouest.org>     --
+-- License: MIT                                          --
+-- ----------------------------------------------------- --
 
-DROP TABLE IF EXISTS `h_coin_history`;
-DROP TABLE IF EXISTS `h_safe_history`;
-DROP TABLE IF EXISTS `h_profile_history`;
-DROP TABLE IF EXISTS `h_coin`;
-DROP TABLE IF EXISTS `h_safe`;
-DROP TABLE IF EXISTS `h_profile`;
+DROP TABLE IF EXISTS h_coin;
+DROP TABLE IF EXISTS h_key;
+DROP TABLE IF EXISTS h_safe;
+DROP TABLE IF EXISTS h_profile;
 
 -- Contain a user profile with a message and a picture choosen by the user to authenticate the server
-CREATE TABLE `h_profile` (
-  `hp_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `hp_username` TEXT NOT NULL,
-  `hp_fortune` TEXT NOT NULL,
-  `hp_picture` TEXT
+CREATE TABLE h_profile (
+  hp_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  hp_sub TEXT NOT NULL,
+  hp_name TEXT,
+  hp_fortune TEXT,
+  hp_picture TEXT,
+  hp_sign_kid TEXT,
+  hp_deleted INTEGER DEFAULT 0,
+  hp_last_updated TIMESTAMP
 );
-CREATE INDEX `i_hp_username` ON `h_profile`(`hp_username`);
+CREATE INDEX i_hp_username ON h_profile(hp_sub);
 
 -- Meta data about the safe
-CREATE TABLE `h_safe` (
-  `hs_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `hp_id` INTEGER NOT NULL,
-  `hs_name` TEXT NOT NULL,
-  `hs_description` TEXT,
-  `hs_key` TEXT NOT NULL,
-  `hs_deleted` INTEGER DEFAULT 0,
-  FOREIGN KEY(`hp_id`) REFERENCES `h_profile`(`hp_id`) ON DELETE CASCADE
+CREATE TABLE h_safe (
+  hs_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  hp_id INTEGER NOT NULL,
+  hs_name TEXT NOT NULL,
+  hs_display_name TEXT,
+  hs_enc_type TEXT,
+  hs_deleted INTEGER DEFAULT 0,
+  hs_last_updated TIMESTAMP,
+  FOREIGN KEY(hp_id) REFERENCES h_profile(hp_id) ON DELETE CASCADE
+);
+
+-- Safe master key
+CREATE TABLE h_key (
+  hk_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  hs_id INTEGER NOT NULL,
+  hk_type TEXT,
+  hk_name TEXT NOT NULL,
+  hk_display_name TEXT,
+  hk_data TEXT, --  The key is protected in a way known by the client and the user only
+  hk_deleted INTEGER DEFAULT 0,
+  hk_last_updated TIMESTAMP,
+  FOREIGN KEY(hs_id) REFERENCES h_safe(hs_id) ON DELETE CASCADE
 );
 
 -- Coin stored in the safe
-CREATE TABLE `h_coin` (
-  `hc_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `hs_id` INTEGER NOT NULL,
-  `hc_name` TEXT NOT NULL,
-  `hc_data` TEXT, --  The usable data is encoded in a blob, so only the end-user can access it if she/he remembers the master password
-  `hc_deleted` INTEGER DEFAULT 0,
-  FOREIGN KEY(`hs_id`) REFERENCES `h_safe`(`hs_id`) ON DELETE CASCADE
-);
-
--- Profile history access
-CREATE TABLE `h_profile_history` (
-  `hph_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `hp_id` INTEGER NOT NULL,
-  `hph_date_access` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `hph_ip_source` TEXT NOT NULL,
-  `hph_access_type` INTEGER NOT NULL, -- 0: CREATE, 1: READ, 2: UPDATE, 3: DELETE, 4: UPDATE FORTUNE, 5: UPDATE PICTURE
-  FOREIGN KEY(`hp_id`) REFERENCES `h_profile`(`hp_id`)
-);
-
--- Safe history access
-CREATE TABLE `h_safe_history` (
-  `hsh_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `hs_id` INTEGER NOT NULL,
-  `hsh_date_access` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `hsh_ip_source` TEXT NOT NULL,
-  `hsh_access_type` INTEGER NOT NULL, -- 0: CREATE, 1: READ, 2: UPDATE, 3: DELETE, 4: UPDATE KEY
-  FOREIGN KEY(`hs_id`) REFERENCES `h_safe`(`hs_id`)
-);
-
--- Coin history access
-CREATE TABLE `h_coin_history` (
-  `hch_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `hc_id` INTEGER NOT NULL,
-  `hch_date_access` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `hch_ip_source` TEXT NOT NULL,
-  `hch_access_type` INTEGER NOT NULL, -- 0: CREATE, 1: READ, 2: UPDATE, 3: DELETE
-  FOREIGN KEY(`hc_id`) REFERENCES `h_coin`(`hc_id`)
+CREATE TABLE h_coin (
+  hc_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  hs_id INTEGER NOT NULL,
+  hc_name TEXT NOT NULL,
+  hc_data TEXT, --  The usable data is encoded in a JWE, so only the end-user can access it if she/he has the master key
+  hc_deleted INTEGER DEFAULT 0,
+  hc_last_updated TIMESTAMP,
+  FOREIGN KEY(hs_id) REFERENCES h_safe(hs_id) ON DELETE CASCADE
 );
